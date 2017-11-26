b32436b65264bbdad7088b7757ff97c4dd0eae0d
Anu Devarmanai <adevar2@illinois.edu>
2017-11-26 01:16:58 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import MapView from './components/map_view/map_view.js'
+import Interests from './components/interests/interests.js'
 
 class App extends React.Component {
     
@@ -11,7 +12,7 @@ class App extends React.Component {
                     <Switch>
                         <Route path="/map-view" component={MapView} />
                         <Route path="/signup" component={null}/>
-                        <Route path="/interests" component={null}/>
+                        <Route path="/interests" component={Interests}/>
                         <Route path="/preferences" component={null}/>
                         <Route path="/feeling-sponataneous" component={null}/>
                         <Route path="/" component={null}/>  {/*This would be landing page*/}
