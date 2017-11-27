e89cc8b7a3c47d8f77a12baa76ade0124ae91c91
nanadav2 <nanadav2@illinois.edu>
2017-11-27 02:31:02 -0600

    folder for preferences_view

---
+++ src/components/preferences_view/preferences_view.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Component} from 'react'
+
+export default class PreferencesView extends Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
