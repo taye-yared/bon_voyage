39b8238630c622c8fd997652ffe67f116b6ac2a5
Anu Devarmanai <adevar2@illinois.edu>
2017-11-28 02:15:54 -0600

    change pic on click

--- src/components/interests/activityPics.js
+++ src/components/interests/activityPics.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import {Component} from 'react'
 import { Image, Button, Checkbox, Icon, Table, Input, Label } from 'semantic-ui-react'
+import pic3 from './picture2.jpg';
+import pic4 from './picture3.jpg';
 const pic1 = require('./picture0.jpg')
 const pic2 = require('./picture1.jpg')
 
@@ -11,16 +13,26 @@ export default class Pics extends Component{
 
         this.state={		
             // Initialize state here if neded
+		picA: pic1, picB: pic2, counter:0
         }
+	this.toggleIcon = this.toggleIcon.bind(this);
+    }
+
+    toggleIcon(){
+	if (this.state.counter == 0){
+		this.setState({picA: pic3});
+		this.setState({picB: pic4});	
+	}
+	this.setState({counter: this.state.counter+1});	
     }
 
     render(){
         // Function will be called whenever the state or props of this class changes.
         // Never call setState in this function! (Will cause an infinite loop)
         return(
-            <div>
-		test
-			
+            <div onClick={this.toggleIcon} >
+		 <img style={{width: '400px', height: '400px'}} src={this.state.picA} />	
+		<img style={{width: '400px', height: '400px'}} src={this.state.picB}  /> 		
             </div>
         )
     }
